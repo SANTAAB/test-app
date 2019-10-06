4d5d26fffc14d88bacfedad78aba5a1ea58dd6de
Alexander Byrka <alexander.byrka@sigma.software>
2019-10-07 00:35:14 +0300

    Updates

---
+++ precache-manifest.424edb15509d560baddd58371618a342.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b718dab0eaf2da0b15bffc86655c91c",
+    "url": "/test-app/index.html"
+  },
+  {
+    "revision": "33caad4aa0a95bc9a4b4",
+    "url": "/test-app/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "d850fda62459df2e1785",
+    "url": "/test-app/static/js/2.bbb5ab8c.chunk.js"
+  },
+  {
+    "revision": "33caad4aa0a95bc9a4b4",
+    "url": "/test-app/static/js/main.e72ae776.chunk.js"
+  },
+  {
+    "revision": "7940372b3d53d0b4d0bb",
+    "url": "/test-app/static/js/runtime-main.52287815.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/test-app/static/media/logo.25bf045c.svg"
+  }
+]);
